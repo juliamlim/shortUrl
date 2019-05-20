f05e29a085f0c09c87340055561447e3ec10aa17
Julia Lim <juliamlim@hotmail.com>
2019-05-20 19:20:37 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Mock backend was created with [mockaroo.com](https://mockaroo.com)
+
+Set up 
+```
+npm i && npm run watch
+```
+
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
 
 import Database from './database';
 import { UrlDetails, UrlInput, UrlList } from './components';
